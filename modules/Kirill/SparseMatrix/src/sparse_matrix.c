cb4afeec6953fbc163c66579697e0bae5eeb5d2c
Kirill <Petrovich_1811@mail.ru>
2016-10-30 15:55:48 +0300

    распаралелен с OpenMP сложение

--- modules/Kirill/SparseMatrix/src/sparse_matrix.c
+++ modules/Kirill/SparseMatrix/src/sparse_matrix.c
@@ -32,6 +32,7 @@ void multMV(TYPE** result, spMatrix mat, TYPE* vec) {
 }
 
 void sum(int N, double h, TYPE **result, TYPE *U, TYPE *k1, TYPE *k2, TYPE *k3, TYPE *k4) {
+#pragma omp parallel for num_threads(2) if (ENABLE_PARALLEL)
   for (int i = 0; i < N; i++)
     (*result)[i] = U[i] + h*(k1[i] + 2*k2[i] + 2*k3[i] + k4[i]);
 }
