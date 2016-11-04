73116b263da0bb362e9284faa480442726835732
Kirill <Petrovich_1811@mail.ru>
2016-11-05 00:12:08 +0300

    отрисована погрешность

--- result/fault.py
+++ result/fault.py
@@ -5,6 +5,8 @@ import numpy
 import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import QFileDialog, QApplication
 
+from matplotlib.ticker import FuncFormatter
+
 path = os.path.abspath(os.path.join(os.path.dirname(__file__),
 									"..", "initial", "INPUT.txt"))
 file = open(path, 'r')
@@ -29,6 +31,7 @@ file, ok = dialog.getOpenFileName(None, 'Первый файл',
                                   '/home/kirill/heat-distribution/result',
                                   'Text files (*.txt)')
 
+
 array1 = numpy.loadtxt(file)
 
 file, ok = dialog.getOpenFileName(None, 'Второй файл',
@@ -38,24 +41,40 @@ file, ok = dialog.getOpenFileName(None, 'Второй файл',
 array2 = numpy.loadtxt(file)
 
 yAbs = [xi-xj for xi, xj in zip(array1, array2)]
+yRelat = [(xi-xj)/max(xi, xj) for xi, xj in zip(array1, array2)]
 
-absoluteFault = max(yAbs)
-relativeFault = max(abs(xi-xj)/max(xi, xj) for xi, xj in zip(array1, array2))
-print ("абсолютная:	%.15f" % absoluteFault)
-print ("относительная:	%.15f" % relativeFault)
+absoluteFault = max(map(abs,yAbs))
+relativeFault = max(map(abs,yRelat))
+print("абсолютная:\t%.15f" % absoluteFault)
+print("относительная:\t%.15f" % relativeFault)
 
 try:
     assert len(x) == len(yAbs)
     # Рисование графиков
-    plt.plot(x, yAbs, label ='absolut')
-    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-           ncol=2, mode="expand", borderaxespad=0.)
-    plt.xlabel('x', fontsize=14)
-    plt.ylabel('abs', fontsize=14)
+
+    plt.figure(num = 'FAULT', facecolor = (1, 1, .54))
+
+    plt.subplot(121)
+    plt.plot(x, yAbs, label ='absolut', color = 'green')
+
+    plt.legend(loc = 2)
+    plt.xlabel('x', fontsize = 14)
     plt.grid(True)
+    ax = plt.gca()
+    f = lambda x,y: '{:.15e}'.format(x)
+    ax.yaxis.set_major_formatter(FuncFormatter(lambda x,y: '{:.0e}'.format(x)))
     # plt.yscale('log')
+
+
+    plt.subplot(122)
+    plt.plot(x, yRelat, label = 'relative', color = 'red')
+    plt.legend(loc = 2)
+    plt.xlabel('x', fontsize=14)
+    plt.grid(True)
+
     plt.show()
 except AssertionError:
-	print ('ERROR! Не совпадают размерности!')
+    print ('ERROR! Не совпадают размерности!')
 
 sys.exit(app.exec_())
+
