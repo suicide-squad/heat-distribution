60b1e32ccb19335eb179a7aeb06c2f51d9721ff5
Lenferd <DeysonSH@gmail.com>
2016-11-16 23:53:32 +0300

    kurwa

--- modules/Sergey/SparseMatrix/src/main_Runge.cpp
+++ modules/Sergey/SparseMatrix/src/main_Runge.cpp
@@ -11,6 +11,7 @@ using std::string;
 
 int main(int argc, char** argv) {
 
+
     // Timing variables
     double time_S, time_E;  // Time for allocate memory
 
@@ -85,35 +86,40 @@ int main(int argc, char** argv) {
     double* v_k4 = new double[nX + 2];
 
 
-    SparseMatrix sm_k1(nX * 3 + 2, nX + 2);
+    SparseMatrix sm_k1;
+    spMatrixInit(sm_k1, nX * 3 + 2, nX + 2);
     double expression1 = sigma / (step * step);
     double expression2 = -2.0 * expression1;
-    sm_k1.fillMatrix2Expr(nX + 2, expression1, expression2);
+    fillMatrix2Expr(sm_k1, nX + 2, expression1, expression2);
 
-    SparseMatrix sm_k2(nX * 3 + 2, nX + 2);
+    // KURWA
+    SparseMatrix sm_k2;
+    spMatrixInit(sm_k2, nX * 3 + 2, nX + 2);
     double k2expr1 = dt * expression1 * 0.5;
     double k2expr2 = 1 - 2.0 * k2expr1;
-    sm_k2.fillMatrix2Expr(nX+2, k2expr1, k2expr2);
+    fillMatrix2Expr(sm_k2, nX+2, k2expr1, k2expr2);
 
-    SparseMatrix sm_k3(nX * 3 + 2, nX + 2);
+    SparseMatrix sm_k3;
+    spMatrixInit(sm_k3, nX * 3 + 2, nX + 2);
     double k3expr1 = k2expr1;
     double k3expr2 = k2expr1;
-    sm_k3.fillMatrix2Expr(nX+2, k3expr2, k3expr2);
+    fillMatrix2Expr(sm_k3, nX+2, k3expr1, k3expr2);
 
-    SparseMatrix sm_k4(nX * 3 + 2, nX + 2);
+    SparseMatrix sm_k4;
+    spMatrixInit(sm_k4, nX * 3 + 2, nX + 2);
     double k4expr1 = dt * expression1;
     double k4expr2 = 1 - 2.0 * k4expr1;
-    sm_k4.fillMatrix2Expr(nX+2, k4expr2, k4expr2);
+    fillMatrix2Expr(sm_k4, nX+2, k4expr1, k4expr2);
 
 
     // Calculating
 
     double expressionResult = dt / 6;
     for (double j = 0; j < tFinal; j += dt) {
-        sm_k1.multiplicateVector(vect[prevTime], v_k1, nX+2);
-        sm_k2.multiplicateVector(v_k1, v_k2, nX+2);
-        sm_k3.multiplicateVector(v_k2, v_k3, nX+2);
-        sm_k4.multiplicateVector(v_k3, v_k4, nX+2);
+        multiplicateVector(sm_k1, vect[prevTime], v_k1, nX+2);
+        multiplicateVector(sm_k2, v_k1, v_k2, nX+2);
+        multiplicateVector(sm_k3, v_k2, v_k3, nX+2);
+        multiplicateVector(sm_k4, v_k3, v_k4, nX+2);
 
         // Fill result vector
         for (int i = 1; i <= nX; i++) {
