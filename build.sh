73ee891615da0c89c17ce466e55e458cf72ad031
Kirill <Petrovich_1811@mail.ru>
2016-11-24 19:40:23 +0300

    Написан мини скрпит для авто-запуска выбранного приложения

--- build.sh
+++ build.sh
@@ -1,22 +1,61 @@
-#!/bin/bash
+#!/bin/sh
 
-sh ./clear.sh
+Build () {
+	Clear
 
-echo "build..."
+	local dir_cmake=$1
+	local target=$2
 
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=RELEASE ../modules/Kirill/SpMatrix
+	echo $dir_cmake
+	echo $target
 
-cp ./../initial/INPUT.txt ./app/
+	mkdir build
+	cd build
 
-echo "done."
+	mkdir modules
+	cd modules
 
-echo "comliler..."
+	cmake -DCMAKE_BUILD_TYPE=RELEASE ../../$dir_cmake
 
-make
+	cp ./../initial/INPUT.txt ./
+	make -j2 $target
 
-echo "done."
+	cd -- "$(find -name "$target" -type f -printf '%h' -quit)"
+	./$target
+}
 
-cd app/euler
-./euler_SpMatrix
+Clear() {
+	if [ -d build ]
+		then
+		rm -rf ./build
+	fi
+}
+
+OPT=$1
+MODUL=$2
+
+case $OPT in
+	-K|-k)
+		case $MODUL in
+			1)
+				Build modules/Kirill/Euler Euler
+				;;
+			2)
+				Build modules/Kirill/RungeKutta RungeKutta
+				;;
+			3)
+				Build modules/Kirill/SpMatrix euler_SpMatrix
+				;;
+			4)
+				Build modules/Kirill/SpMatrix runge_SpMatrix
+				;;
+			*)
+				echo "'1' - Euler; '2' - RungeKutta; '3' - SpMatrix Euler; '4' - SpMatrix RungeKutta" 
+
+		esac
+		;;
+	*)
+		echo "'-K' - Kirill" 
+    	exit 1
+    	;; 
+esac
