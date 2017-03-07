922bcbe531b4acf01ddd9f8f3b3950ccb653079d
Kirill <Petrovich_1811@mail.ru>
2017-03-08 02:37:30 +0300

    Почищено лишнее. Реализован адекватный вывод

--- sustainability study/plot.py
+++ sustainability study/plot.py
@@ -3,6 +3,7 @@ import re
 import sys
 import numpy
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 
 def fault(file1, file2):
     array1 = numpy.loadtxt(file1)
@@ -13,36 +14,44 @@ def fault(file1, file2):
     absoluteFault = max(yAbs)
     return absoluteFault
 
-directory = os.path.abspath(os.path.join(os.path.dirname(__file__),
-									"KEKUREKUS", "Jakobi_Euler"))
+methods = os.listdir(os.path.join(os.path.dirname(__file__),
+									"KEKUREKUS"))
 
-fileDefault = os.path.join(directory, "OUTPUT_JACOBI_.txt")
+print(methods)
+for method in methods:
+    directory = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        "KEKUREKUS", method))
 
-files = [os.path.join(directory, file) for file in os.listdir(directory)
-         if file[-4:] == '.txt' and file != "OUTPUT.txt" and file != "OUTPUT_JACOBI_.txt"]
+    files = [os.path.join(directory, file) for file in os.listdir(directory)
+             if file[-4:] == '.txt']
 
-print(files)
-number = re.compile(r'\d+')
-numbers = [int(number.findall(file)[0]) for file in files]
+    number = re.compile(r'\d+\.?\d*')
+    numbers = [float(number.findall(file)[0]) for file in files]
 
-absoluteFaults = [fault(file, fileDefault) for file in files]
+    dic = dict(zip(numbers, files))
 
-#####################################################################
-#                    Рисование графика                              #
-#####################################################################
+    fileDefault = dic.get(max(numbers))
 
-plt.figure(num = 'FAULT', facecolor = (1, 1, .54))
+    numbers.sort()
+    files = [dic[i] for i in numbers]
 
-plt.plot(numbers, absoluteFaults, 'ro',  label ='absolut', color = 'green')
-plt.yscale('symlog')
+    absoluteFaults = [fault(file, fileDefault) for file in files]
 
-# plt.legend(loc = 2)
-# plt.xlabel('x', fontsize = 14)
-# plt.grid(True)
-# ax = plt.gca()
-# f = lambda x,y: '{:.15e}'.format(x)
-# ax.yaxis.set_major_formatter(FuncFormatter(lambda x,y: '{:.0e}'.format(x)))
+    #####################################################################
+    #                    Рисование графика                              #
+    #####################################################################
 
-plt.show()
+    plt.figure(num = method, facecolor = (1, 1, .54))
 
-# print(dic)
+    plt.plot(numbers, absoluteFaults,'bo',  label = method, color = 'black')
+    plt.plot(numbers, absoluteFaults,':k', color = 'green')
+    # plt.yscale('log')
+
+    plt.legend(loc = 1)
+    plt.xlabel('dt', fontsize = 14)
+    plt.grid(True)
+    ax = plt.gca()
+    f = lambda x,y: '{:.15e}'.format(x)
+    ax.yaxis.set_major_formatter(FuncFormatter(lambda x,y: '{:.0e}'.format(x)))
+    plt.savefig(os.path.join('plots', method + '.png'))
+    # plt.show()
