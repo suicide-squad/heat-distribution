a77c7dec49f4ee9d2405ac5e3b94886afab9de6c
Kirill <Petrovich_1811@mail.ru>
2017-03-08 17:42:53 +0300

    Добавлены моя реализация файлов Неявной схеме

--- sustainability study/build_plots.py
+++ sustainability study/build_plots.py
@@ -14,13 +14,13 @@ def fault(file1, file2):
     absoluteFault = max(yAbs)
     return absoluteFault
 
-methods = os.listdir(os.path.join(os.path.dirname(__file__),
-									"KEKUREKUS"))
+dirMethods = os.path.join(os.path.dirname(__file__), "KEKUREKUS")
+methods = os.listdir(dirMethods)
 
 print(methods)
+# Для каждой папки строяться по файлам графики!
 for method in methods:
-    directory = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                        "KEKUREKUS", method))
+    directory = os.path.join(dirMethods, method)
 
     files = [os.path.join(directory, file) for file in os.listdir(directory)
              if file[-4:] == '.txt']
@@ -43,15 +43,16 @@ for method in methods:
 
     plt.figure(num = method, facecolor = (1, 1, .54))
 
-    plt.plot(numbers, absoluteFaults,'bo',  label = method, color = 'black')
+    plt.plot(numbers, absoluteFaults,'bo',  label = method, color = 'black',)
     plt.plot(numbers, absoluteFaults,':k', color = 'green')
     # plt.yscale('log')
 
     plt.legend(loc = 1)
-    plt.xlabel('dt', fontsize = 14)
+    plt.xlabel('timesize', fontsize = 12)
     plt.grid(True)
     ax = plt.gca()
-    f = lambda x,y: '{:.15e}'.format(x)
     ax.yaxis.set_major_formatter(FuncFormatter(lambda x,y: '{:.0e}'.format(x)))
+    ax.xaxis.set_major_formatter(FuncFormatter(lambda x,y: '2^{:.0f}'.format(x)))
+
     plt.savefig(os.path.join('plots', method + '.png'))
     # plt.show()
